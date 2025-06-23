ebd3beb39943d68223e39ac9b1784682281ed0f4
Tanishq <tanishqsangwan7@gmail.com>
2025-06-23 22:10:18 +0530

    Touchups

--- src/components/AboutModal/index.jsx
+++ src/components/AboutModal/index.jsx
@@ -5,12 +5,13 @@ import {Modal} from "../index.js";
 const AboutModal = ({ show, onClose }) => {
 
   return (
-    <Modal show={show} onClose={onClose} title={"About"}>
+    <Modal show={show} onClose={onClose} title={"About"} contentClass={"aboutmodal"}>
       <p>In 2015, Rora Blue started <a href="https://theunsentproject.com/">the Unsent Project</a> to figure out what color people see love in, in this, people submitted colors the people associated with their first love. <a
-        href="https://theunsentproject.com/about">Read more here</a></p>
-      <p>Highly inspired from that, I built this, <b>The Unspoken Project</b> - an interactive wall of anonymous messages — a version of The Unsent Project, reimagined through my eyes, beyond just love letters.</p>
+        href="https://theunsentproject.com/about">Read more</a></p>
+      <p>Highly inspired from that, I, Tanishq Sangwan, built this, <b>The Unspoken Project</b> - an interactive wall of anonymous messages — a version of The Unsent Project, reimagined through my eyes, beyond just love letters.</p>
       <p>Where The Unsent Project collects unsent texts to past lovers, The Unspoken Project invites users to express <b>anything they've never been able to say</b> — from friendship, betrayal, and surrender to deep secrets and silent grief — all anonymously.</p>
       <p>Every note appears on an almost (infinite) canvas, with it's emoji and message, forming a universe of what was left unspoken.</p>
+      <p>Github link: <a href="http://github.com/sadashii/TheUnspokenProject">here</a></p>
     </Modal>
   )
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,6 +6,7 @@ import { AboutModal, EmojiModal } from "../index.js";
 const Navbar = ({ onClickCreate, notes, OFFSET }) => {
   const [showAbout, setShowAbout] = useState(false)
   const [showEmojis, setShowEmojis] = useState(false)
+  const [hamburgerToggled, setHamburgerToggled] = useState(false)
   const {setTransform} = useControls()
   
   const zoomToNote = (note) => {
@@ -24,6 +25,12 @@ const Navbar = ({ onClickCreate, notes, OFFSET }) => {
     let note = notes[random_note];
     zoomToNote(note)
   }
+
+  const onClickMobileMenuOutside = (e) => {
+    if (e.target == e.currentTarget) {
+      setHamburgerToggled(false)
+    }
+  }
   
   
   return (
@@ -33,15 +40,34 @@ const Navbar = ({ onClickCreate, notes, OFFSET }) => {
           <img src="./logo_black.png" alt="The Unspoken Project logo"/>
           The<br/>Unspoken<br/>Project
         </div>
-        <div className="options">
+        <div className="options-desktop">
           <div className="option" onClick={() => setShowAbout(true)}>About</div>
           <div className="option" onClick={() => setShowEmojis(true)}>The Emojis</div>
           <div className="option" onClick={visitRandomNote}>Random</div>
           <div className="post_a_message" onClick={onClickCreate}>Post a message</div>
         </div>
+        <div className={`options-mobile ${hamburgerToggled ? 'options-shown' : ''}`} onClick={() => setHamburgerToggled(p => !p)}>
+            <div className="ham ham1"></div>
+            <div className="ham ham2"></div>
+            <div className="ham ham3"></div>
+        </div>
       </nav>
-      <AboutModal show={showAbout} onClose={() => setShowAbout(false)} />
-      <EmojiModal show={showEmojis} onClose={() => setShowEmojis(false)} />
+
+      <div className={`mobile-menu-wrapper ${hamburgerToggled ? 'mobile-menu-shown' : ''}`} onClick={onClickMobileMenuOutside}>
+        <div className="mobile-menu-content">
+          <div className="icon">
+            <img src="./logo_black.png" alt="The Unspoken Project logo"/>
+            The<br/>Unspoken<br/>Project
+          </div>
+          <div className="option" onClick={() => {setHamburgerToggled(false);setShowAbout(true)}}>About</div>
+          <div className="option" onClick={() => {setHamburgerToggled(false);setShowEmojis(true)}}>The Emojis</div>
+          <div className="option" onClick={() => {setHamburgerToggled(false);visitRandomNote()}}>Random</div>
+          <div className="post_a_message" onClick={() => {setHamburgerToggled(false);onClickCreate()}}>Post a message</div>
+        </div>
+      </div>
+
+      <AboutModal show={showAbout} onClose={() => setShowAbout(false)}/>
+      <EmojiModal show={showEmojis} onClose={() => setShowEmojis(false)}/>
     </>
   )
 }
